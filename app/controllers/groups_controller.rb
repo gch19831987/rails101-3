b47a51c6029178fc905071be459d7e154bdcaa05
zqz <71264737@qq.com>
2017-01-03 14:43:03 +0800

    implement groups#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -25,6 +25,18 @@ class GroupsController < ApplicationController
 		end
 	end
 
+	def update
+		
+		@group = Group.find(params[:id])
+
+		if @group.update(group_params)
+			redirect_to groups_path
+		else 
+			render :edit
+
+		end
+	end
+
 	private
 	def group_params
 		params.require(:group).permit(:title,:description)
